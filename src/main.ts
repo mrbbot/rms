cfc1d95789d3dd46f4c4e0cda7e138df8d978395
MrBBot <me@mrbbot.dev>
2021-02-02 21:56:13 +0000

    Warn before unload

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+  e.returnValue = "";
+});
