3fd709660d4a1df9577506b49ab2052390d8263b
MrBBot <me@mrbbot.dev>
2021-02-04 12:19:57 +0000

    Persist after loading from hash

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import "modern-normalize/modern-normalize.css";
 import { createApp } from "vue";
 import App from "./App.vue";
-import { load, loadPersisted } from "./components/data";
+import { load, loadPersisted, persist } from "./components/data";
 
 // Try to load data from hash
 let loadPromise: Promise<void> | undefined = undefined;
 if (window.location.hash.length > 0) {
   try {
     load(JSON.parse(atob(window.location.hash.substring(1))));
+    persist();
     loadPromise = Promise.resolve();
   } catch (e) {
     console.error("Error parsing hash:", e);
